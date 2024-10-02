3c4e27680a25543e037145263e3b52fd3d39893f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-03 00:32:36 +0300

    Add A DbContext

---
+++ ProjectApI/Data/Context.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectApI.Models;
+
+namespace ProjectApI.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> products  { get; set; }
+        public DbSet<Cart> carts { get; set; }
+        public DbSet<CartItem> cartItems { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Payment> payments { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<WishList> wishLists { get; set; }
+        public DbSet<WishListItem> wishListItems { get; set; }   
+
+
+
+
+        public Context()
+        {
+             
+        }
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=WebApi;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        }
+    }
+}
