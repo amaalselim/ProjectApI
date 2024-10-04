37d8cdb73b61631c91890c17d6029f9ef4658fd4
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-05 00:45:45 +0300

    add scoped to unitofwork and addCors and JWT

--- ProjectApI/Program.cs
+++ ProjectApI/Program.cs
@@ -1,6 +1,14 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ProjectApI.Data;
+using ProjectApI.Models;
+using ProjectAPI.IRepository;
+using ProjectAPI.Repository;
+using System.Text;
 
 namespace ProjectApI
 {
@@ -14,10 +22,92 @@ namespace ProjectApI
             builder.Services.AddDbContext<Context>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("cs")));
 
+            //builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<Context>().AddDefaultTokenProviders();
+
             builder.Services.AddControllers();
+
+
+            builder.Services.AddAuthentication(
+                options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                }
+                ).AddJwtBearer(
+                options => {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = builder.Configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"])),
+                    };
+                });
+                builder.Services.AddCors(corsOptions =>
+                {
+                    corsOptions.AddPolicy("MyPolicy", policyBuilder =>
+                    {
+                        policyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                    });
+                });
+
+
+            /* builder.Services.AddAuthorization(options =>
+             {
+                 options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+                 options.AddPolicy("User", policy => policy.RequireRole("User"));
+             });*/
+
+
+            /*-----------------------------Swagger Part-----------------------------*/
+            #region Swagger REgion
+            //builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation    
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ASP.NET 5 Web API",
+                    Description = " ITI Projrcy"
+                });
+                // To Enable authorization using Swagger (JWT)    
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                    {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                    }
+                    },
+                    new string[] {}
+                    }
+                    });
+            });
+            #endregion
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+            builder.Services.AddHttpContextAccessor();  
 
             var app = builder.Build();
 
@@ -27,7 +117,8 @@ namespace ProjectApI
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseCors("MyPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
