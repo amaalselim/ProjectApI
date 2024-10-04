3bbf03e9e5a26ab96536aabe2d6903afe2253762
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-05 00:45:19 +0300

    Create and Implement UnitOfWork

---
+++ ProjectApI/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ProjectApI.IRepository;
+
+namespace ProjectAPI.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepo  CategoryRepo { get; }
+        IProductRepo productRepo { get; }
+        ICartRepo CartRepo { get; }
+        IOrderRepo OrderRepo { get; }
+        IPaymentRepo PaymentRepo { get; }  
+        IUserRepo UserRepo { get; } 
+        IWishListRepo WishListRepo { get; }
+        int Complete();
+
+    }
+}
---
+++ ProjectApI/Repository/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using ProjectApI.Data;
+using ProjectApI.IRepository;
+using ProjectApI.Repository;
+using ProjectAPI.IRepository;
+
+namespace ProjectAPI.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context _context;
+        public ICategoryRepo CategoryRepo {  get; private set; }    
+
+        public IProductRepo productRepo { get; private set; }
+
+        public ICartRepo CartRepo { get; private set; }
+
+        public IOrderRepo OrderRepo { get; private set; }
+
+        public IPaymentRepo PaymentRepo { get; private set; }
+
+        public IUserRepo UserRepo { get; private set; }
+
+        public IWishListRepo WishListRepo { get; private set; }
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+            CategoryRepo = new CategoryRepo(context);
+            productRepo = new ProductRepo(context);
+            CartRepo = new CartRepo(context);
+            OrderRepo = new OrderRepo(context);
+            PaymentRepo = new PaymentRepo(context);
+            UserRepo = new UserRepo(context);
+            WishListRepo = new WishListRepo(context);
+        }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
