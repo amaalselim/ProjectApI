7146256e528b4c296a1fcc8aeb71410da8f9470a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-10 14:19:59 +0300

    Add Migrations

---
+++ ProjectApI/Migrations/20241009155726_product.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectApI.Migrations
+{
+    /// <inheritdoc />
+    public partial class product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
