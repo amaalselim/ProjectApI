28a1c9551e0b52972b72d402faa6e11b89415d8b
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-10-10 14:18:59 +0300

    Create PaymentController

---
+++ ProjectApI/Controllers/PaymentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectApI.Models;
+using ProjectAPI.IRepository;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PaymentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpPost("Process")]
+        [Authorize]
+
+        public async Task<IActionResult> ProcessPayment(Payment payment)
+        {
+            var Processpayment = await _unitOfWork.PaymentRepo.ProcessPaymentAsync(payment);
+            return Ok(Processpayment);
+        }
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetPaymentById(int id)
+        {
+            var payment = await _unitOfWork.PaymentRepo.GetPaymentByIdAsync(id);
+            if (payment == null)
+            {
+                return NotFound("Payment Not Found");
+            }
+            return Ok(payment);
+        }
+        
+    }
+}
